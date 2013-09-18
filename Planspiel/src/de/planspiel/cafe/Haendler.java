a1215ac97309ba5aecd5281d79497084f66177c5
Natalie <Natalie@192.168.178.23>
2013-09-18 17:16:31 +0200

    Habe mal einen groben ersten Vorschlag für die angebotGenerieren()-Methode gemacht.

--- Planspiel/src/de/planspiel/cafe/Haendler.java
+++ Planspiel/src/de/planspiel/cafe/Haendler.java
@@ -11,15 +11,30 @@ public class Haendler {
 	}
 	
 	public void generierenAngebot() {
-		// TODO
+		// TODO Entscheidung: Wie viele Produkte bietet ein Händler an? Evtl. Schleife und Produktnamen in ENUM speichern
+		//Konstruktor: Produkt (name, qualitaet, ekpreis) 
+		produktListe.add(new Produkt("Kaffee", zufallszahl(10), zufallszahl(20)) );  // Abhängigkeit Preis von Qualität?
+		produktListe.add(new Produkt("Tee", zufallszahl(10), zufallszahl(20)));
+		produktListe.add(new Produkt("Kakao", zufallszahl(10), zufallszahl(20)));
 	}
 	
 	public Vector<Produkt> ausgebenAngebot() {
-		// TODO
+		// TODO für die GUI? entspricht doch eigentlich holeProduktListe?
 		return null;
 	}
 	
 	public Vector<Produkt> holeProduktListe() {
 		return produktListe;
+	}
+	
+	/*
+	 * Zufallszahl zwischen 0 und Grenzen mit zwei Nachkommastellen erzeugen - optional auch mit Untergrenze
+	 */
+	private double zufallszahl(int grenze){
+		double zufallszahl = Math.random()*grenze;
+		int zufallszahl100 = (int) zufallszahl * 100;
+		zufallszahl = zufallszahl100 / 100;
+		
+		return zufallszahl;
 	}
 }
