246aa29b19cebf7520d9d227b4a3fdaef37b28f6
Natalie <nataliebuchner@aol.com>
2013-09-23 14:58:42 +0200

    Lager geupdatet (sucheProdukt)

--- Planspiel/src/de/planspiel/cafe/Lager.java
+++ Planspiel/src/de/planspiel/cafe/Lager.java
@@ -10,17 +10,36 @@ public class Lager {
 		produktListe = new Vector<Produkt>();
 	}
 	
+	/**
+	 * 
+	 * @param produkt
+	 */
 	public void einlagern(Produkt produkt) {
 		// TODO
+		//Falls Produkt noch nicht vorhanden
+		produktListe.add(produkt);
+		//Andernfalls schon vorhandenes Produkt mit neuem verschmelzen
+		
 	}
 	
-	public Produkt auslagern(String name, int menge) {
-		// TODO
+	public Produkt auslagern(Produkttypen name, int menge) {
+		//TODO
 		return null;
 	}
 	
-	public Produkt suchenProdukt(String name) {
-		// TODO
+	/**
+	 * Sucht in den vorhandenen Produkten, ob das übergebene Produkt (nach Name) schon existiert
+	 * @param name Produkttypen-Wert, der angibt welches Produkt gesucht wird
+	 * @return Null falls das Produkt noch nicht existiert, Produkt wenn es gefunden wurde
+	 * @author Natalie
+	 */
+	public Produkt suchenProdukt(Produkttypen name) {
+		for (int i = 0; i < produktListe.size(); i++){
+			if (produktListe.get(i).holeName() == name){
+				return produktListe.get(i);
+			}
+		}
+		//falls Produkt nicht vorhanden
 		return null;
 	}
 	
