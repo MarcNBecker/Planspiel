7cd2ab22b174e3d635a1b2fb0b2c2fcd2c567a29
Natalie <nataliebuchner@aol.com>
2013-09-23 18:25:21 +0200

    FilialeUpdate

--- Planspiel/src/de/planspiel/cafe/Filiale.java
+++ Planspiel/src/de/planspiel/cafe/Filiale.java
@@ -6,66 +6,94 @@ public class Filiale {
 	private Standort standort;
 	private Unternehmenskette kette;
 	private int freieKapazitaet;
-	
-	public Filiale(Standort standort, Unternehmenskette kette){
+
+	public Filiale(Standort standort, Unternehmenskette kette) {
 		this.standort = standort;
 		this.kette = kette;
 		this.mitarbeiter = 0;
 		this.freieKapazitaet = 0;
 	}
 	
-	public int verkaufen(String name, int menge) {
-		// TODO
-		return 0;
+
+	/**
+	 * Gibt die Menge zurück, die tatsächlich an den Kunden verkauft wurde.
+	 * @param name
+	 *            Produkttyp, der verkauft werden soll
+	 * @param menge
+	 *            Menge, die von dem Produkttypen verkauft werden soll
+	 * @return Menge, die tatsächlich verkauft wird (z.B. bei Minderbestand)
+	 */
+	public int verkaufen(Produkttyp name, int menge) {
+		Produkt lagerProdukt = this.holeKette().holeLager().auslagern(name, menge);
+		// TODO kette.verbuchenUmsatz();
+		return lagerProdukt.holeMenge();
 	}
-	
+
+	/**
+	 * Prüft, ob das gewünschte Produkt vorhanden ist. Dabei werden die Eigenschaften (Qualität, etc.) berücksichtigt.
+	 * @param produkt
+	 *            Ist das gewollte Produkt vom Kunden. Es wird geprüft, ob das
+	 *            Produkt im Lager ist und ob die Eigenschaften (Qualität, etc.)
+	 *            stimmen.
+	 * @return Gibt zurück, ob ein Produkt vorhanden ist und, ob die
+	 *         gewollten Eigenschaften (Qualität, etc.) mit dem Produkt im Lager
+	 *         übereinstimmen.
+	 */
 	public boolean pruefenKundenprodukt(Produkt produkt) {
-		// TODO
+		// Pruefe, ob das Produkt vorhanden ist.
+		Produkt lagerProdukt = this.holeKette().holeLager()
+				.suchenProdukt(produkt.holeName());
+		if (lagerProdukt != null) {
+			// pruefe, ob die Eigenschaften, wie Qualität, übereinstimmen
+			if (produkt.vergleichen(lagerProdukt)) {
+				return true;
+			}
+		}
 		return false;
 	}
-	
-	public void berechnenKapazitaet() {
-		// TODO
+
+	/**
+	 * Verbucht die Gesamtkosten der Filiale.
+	 */
+	public void verbuchenKosten() {
+		// Kosten für die Mitarbeiter
+		double kosten = (this.holeMitarbeiter() * this.holeKette().holeGehalt());
+		this.holeKette().verbuchenKosten(Kostenverursacher.PERSONAL, kosten);
+		// Kosten für den Standort
+		double filialKosten = (this.holeStandort().holeLaufendeFilialkosten());
+		this.holeKette().verbuchenKosten(Kostenverursacher.FILIALE_UNTERHALTUNG, filialKosten);
 	}
-	
-	public void berechnenKosten() {
-		// TODO
+
+	/**
+	 * Muss jede Runde einmal aufgerufen werden.
+	 */
+	public void initialisierenKapazitaet() {
+		this.setzeFreieKapazitaet(this.holeStandort().berechnenKapazitaet(this.holeMitarbeiter()));
 	}
-	
-	public void einstellenMitarbeiter() {
-		// TODO
-	}
-	
-	public void entlassenMitarbeiter() {
-		// TODO
-	}
-	
-	public void holeStartKapazitaet() {
-		// TODO
-	}
-	
+
 	public int holeMitarbeiter() {
-		return mitarbeiter;
+		return this.mitarbeiter;
 	}
-	
+
 	public void setzeMitarbeiter(int mitarbeiter) {
 		this.mitarbeiter = mitarbeiter;
 	}
-	
+
 	public Standort holeStandort() {
-		return standort;
+		return this.standort;
 	}
-	
+
 	public Unternehmenskette holeKette() {
-		return kette;
+		return this.kette;
 	}
-	
+
 	public int holeFreieKapazitaet() {
-		return freieKapazitaet;
+		return this.freieKapazitaet;
 	}
-	
+
 	public void setzeFreieKapazitaet(int freieKapazitaet) {
 		this.freieKapazitaet = freieKapazitaet;
 	}
-	
+
 }
+
