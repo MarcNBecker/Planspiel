8a68ecdd4df05fe24c233eca2dedab847688fb51
Daniel <DanielDegraf@googlemail.com>
2013-09-23 15:49:19 +0200

    Filiale  überarbeitet.

--- Planspiel/src/de/planspiel/cafe/Filiale.java
+++ Planspiel/src/de/planspiel/cafe/Filiale.java
@@ -1,5 +1,11 @@
 package de.planspiel.cafe;
 
+/**
+ * 
+ * @author Daniel Degraf
+ * 
+ */
+
 public class Filiale {
 
 	private int mitarbeiter;
@@ -14,62 +20,76 @@ public class Filiale {
 		this.freieKapazitaet = 0;
 	}
 
-	public int verkaufen(Produkttypen name, int menge) {
-		// TODO
-		
-		return 0;
-	}
 	/**
-	 * @author Daniel
-	 * @param produkt Ist das gewollte Produkt vom Kunden. Es wird geprüft, ob das Produkt im Lager ist und ob die Eigenschaften (Qualität, etc.) stimmen.
-	 * @return boolean Gibt zurück, ob ein Produkt vorhanden ist und, ob die gewollten Eigenschaften (Qualität, etc.) mit dem Produkt im Lager übereinstimmen.
+	 * 
+	 * @param name
+	 *            Produkttyp, der verkauft werden soll
+	 * @param menge
+	 *            Menge, die von dem Produkttypen verkauft werden soll
+	 * @return Menge, die tatsächlich verkauft wird (z.B. bei Minderbestand)
+	 */
+	public int verkaufen(Produkttypen name, int menge) {
+		if (pruefenKundenprodukt(this.holeKette().holeLager()
+				.suchenProdukt(name))==true) {
+			Produkt lagerProdukt = this.holeKette().holeLager()
+					.auslagern(name, menge);
+			return lagerProdukt.holeMenge();
+		} else {
+			return 0;
+		}
+	}
+
+	/**
+	 * @param produkt
+	 *            Ist das gewollte Produkt vom Kunden. Es wird geprüft, ob das
+	 *            Produkt im Lager ist und ob die Eigenschaften (Qualität, etc.)
+	 *            stimmen.
+	 * @return boolean Gibt zurück, ob ein Produkt vorhanden ist und, ob die
+	 *         gewollten Eigenschaften (Qualität, etc.) mit dem Produkt im Lager
+	 *         übereinstimmen.
 	 */
 	public boolean pruefenKundenprodukt(Produkt produkt) {
-		// TODO Wie wirkt sich ein Minderbestand auf den Kunden aus? Wenn er nur 15, statt 20 gewollte bekommt.
 		// Pruefe, ob das Produkt vorhanden ist.
-		Produkt helpProdukt = this.holeKette().holeLager().suchenProdukt(produkt.holeName()); 
-		if (helpProdukt!=null) {
+		Produkt lagerProdukt = this.holeKette().holeLager()
+				.suchenProdukt(produkt.holeName());
+		if (lagerProdukt != null) {
 			// pruefe, ob die Eigenschaften, wie Qualität, übereinstimmen
-			if (produkt.vergleichen(helpProdukt)) {
+			if (produkt.vergleichen(lagerProdukt)) {
 				return true;
 			}
 		}
 		return false;
 	}
 
-	public void berechnenKapazitaet() {
-		// TODO
-	}
-	
 	/**
-	 * @author Daniel
-	 * @return help Double-Wert, der die Höhe der Mitarbeiter- und Standort-Kosten gebündelt zurückgibt.
+	 * @return help Double-Wert, der die Höhe der Mitarbeiter- und
+	 *         Standort-Kosten gebündelt zurückgibt.
 	 */
 	public double berechnenKosten() {
 		// TODO
-		double help = 0.0;
+		double kosten = 0.0;
 		// Kosten für die Mitarbeiter
-		help = (this.holeMitarbeiter() * this.holeKette().holeGehalt());
+		kosten = (this.holeMitarbeiter() * this.holeKette().holeGehalt());
 		// Kosten für den Standort
-		help = help + (this.holeStandort().holeLaufendeFilialkosten());
-		return help;
+		kosten = kosten + (this.holeStandort().holeLaufendeFilialkosten());
+		return kosten;
 	}
 
 	/**
-	 * @author Daniel
-	 * @param anzahl Int-Wert, der die Anzahl der zu einstellenden Mitarbeitern angibt.
+	 * @param anzahl
+	 *            Int-Wert, der die Anzahl der zu einstellenden Mitarbeitern
+	 *            angibt.
 	 */
 	public void einstellenMitarbeiter(int anzahl) {
-		// TODO
 		this.setzeMitarbeiter(this.holeMitarbeiter() + anzahl);
 	}
 
 	/**
-	 * @author Daniel
-	 * @param anzahl Int-Wert, der die Anzahl der zu entlassenden Mitarbeitern angibt.
+	 * @param anzahl
+	 *            Int-Wert, der die Anzahl der zu entlassenden Mitarbeitern
+	 *            angibt.
 	 */
 	public void entlassenMitarbeiter(int anzahl) {
-		// TODO
 		this.setzeMitarbeiter(this.holeMitarbeiter() - anzahl);
 		for (int i = 0; i < anzahl; i++) {
 			this.holeKette().verbuchenKosten(Kostenverursacher.PERSONAL,
@@ -78,7 +98,9 @@ public class Filiale {
 	}
 
 	public void initialisierenKapazitaet() {
-		// TODO
+		// TODO Bitte in Verbindung mit berechneKapazität in der Klasse Standort nochmal prüfen
+		this.setzeFreieKapazitaet(this.holeStandort().berechnenKapazitaet(
+				this.holeMitarbeiter()));
 	}
 
 	public int holeMitarbeiter() {
