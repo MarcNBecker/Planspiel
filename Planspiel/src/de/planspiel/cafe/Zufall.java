84058fa9b72c1387975fc4436645f530c4b9fd6b
Kathi <ann-kathrin.gessat@web.de>
2013-09-25 15:18:01 +0200

    Kommentare

--- Planspiel/src/de/planspiel/cafe/Zufall.java
+++ Planspiel/src/de/planspiel/cafe/Zufall.java
@@ -1,5 +1,12 @@
 package de.planspiel.cafe;
 
+/**
+ * Klasse zur Generierung von Zufallszahlen und zum Treffen von Entscheidungen.
+ * Kann zu Testzwecken verwendet werden, indem Testmodus und Testentscheidung und Testzufallszahl gesetzt werden.
+ * @author Ann-Kathrin Gessat
+ *
+ */
+
 public class Zufall {
 
 	private static boolean testModus = false;
@@ -7,7 +14,7 @@ public class Zufall {
 	private static double testZufallszahl = 0.0;
 
 	/**
-	 * Zufallszahl zwischen 0 und Grenzen mit zwei Nachkommastellen erzeugen -
+	 * Zufallszahl zwischen 0 und Grenze mit zwei Nachkommastellen erzeugen -
 	 * optional auch mit Untergrenze
 	 */
 	public static double generierenZufallszahl(int grenze) {
@@ -19,6 +26,11 @@ public class Zufall {
 		return zufallszahl;
 	}
 
+	/**
+	 * Liefert zurück, ob Entscheidung getroffen wird oder nicht.
+	 * @param wahrscheinlichkeit
+	 * @return true oder false
+	 */
 	public static boolean treffenEntscheidung(double wahrscheinlichkeit) {
 		if (Zufall.holeTestModus()) {
 			return Zufall.holeTestEntscheidung();
@@ -29,26 +41,50 @@ public class Zufall {
 		return false;
 	}
 
+	/**
+	 * 
+	 * @return testModus
+	 */
 	public static boolean holeTestModus() {
 		return Zufall.testModus;
 	}
 
+	/**
+	 * Setzt den Test-Modus auf true oder false.
+	 * @param testmodus
+	 */
 	public static void setzeTestmodus(boolean testmodus) {
 		Zufall.testModus = testmodus;
 	}
 
+	/**
+	 * 
+	 * @return testEntscheidung
+	 */
 	public static boolean holeTestEntscheidung() {
 		return Zufall.testEntscheidung;
 	}
 
+	/**
+	 * Setzt die Test-Entscheidung auf true oder false.
+	 * @param testentscheidung
+	 */
 	public static void setzeTestEntscheidung(boolean testentscheidung) {
 		Zufall.testEntscheidung = testentscheidung;
 	}
 
+	/**
+	 * 
+	 * @return testZufallszahl
+	 */
 	public static double holeTestZufallszahl() {
 		return Zufall.testZufallszahl;
 	}
 
+	/**
+	 * Setzt die Test-Zufallszahl
+	 * @param testzufallszahl
+	 */
 	public static void setzeTestZufallszahl(double testzufallszahl) {
 		Zufall.testZufallszahl = testzufallszahl;
 	}
