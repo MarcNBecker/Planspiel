5f4a7c2e41cc773d3c54f751f1c00c256ea96cdf
Kathi <ann-kathrin.gessat@web.de>
2013-09-25 15:01:10 +0200

    Zufall

--- Planspiel/src/de/planspiel/cafe/Zufall.java
+++ Planspiel/src/de/planspiel/cafe/Zufall.java
@@ -1,6 +1,7 @@
 package de.planspiel.cafe;
 
 public class Zufall {
+
 	private static boolean testModus = false;
 	private static boolean testEntscheidung = false;
 	private static double testZufallszahl = 0.0;
@@ -10,8 +11,8 @@ public class Zufall {
 	 * optional auch mit Untergrenze
 	 */
 	public static double generierenZufallszahl(int grenze) {
-		if (testModus)
-			return testZufallszahl;
+		if (Zufall.holeTestModus())
+			return Zufall.holeTestZufallszahl();
 		double zufallszahl = Math.random() * grenze;
 		int zufallszahl100 = (int) zufallszahl * 100;
 		zufallszahl = zufallszahl100 / 100;
@@ -19,8 +20,8 @@ public class Zufall {
 	}
 
 	public static boolean treffenEntscheidung(double wahrscheinlichkeit) {
-		if (testModus) {
-			return testEntscheidung;
+		if (Zufall.holeTestModus()) {
+			return Zufall.holeTestEntscheidung();
 		}
 		double zufallszahl = generierenZufallszahl(1);
 		if (zufallszahl <= wahrscheinlichkeit)
@@ -28,12 +29,24 @@ public class Zufall {
 		return false;
 	}
 
+	public static boolean holeTestModus() {
+		return Zufall.testModus;
+	}
+
 	public static void setzeTestmodus(boolean testmodus) {
 		Zufall.testModus = testmodus;
 	}
 
+	public static boolean holeTestEntscheidung() {
+		return Zufall.testEntscheidung;
+	}
+
 	public static void setzeTestEntscheidung(boolean testentscheidung) {
 		Zufall.testEntscheidung = testentscheidung;
+	}
+
+	public static double holeTestZufallszahl() {
+		return Zufall.testZufallszahl;
 	}
 
 	public static void setzeTestZufallszahl(double testzufallszahl) {
