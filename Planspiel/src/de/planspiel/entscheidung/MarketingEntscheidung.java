b8656c4bb592bd2337aa3ee1e135bbb45dfad27a
Kathi <ann-kathrin.gessat@web.de>
2013-10-09 10:39:15 +0200

    MarketingEntscheidung

--- Planspiel/src/de/planspiel/entscheidung/MarketingEntscheidung.java
+++ Planspiel/src/de/planspiel/entscheidung/MarketingEntscheidung.java
@@ -1,55 +1,68 @@
 package de.planspiel.entscheidung;
 
-import java.util.Vector;
-
-import de.planspiel.cafe.Filiale;
+import de.planspiel.cafe.Standort;
 import de.planspiel.cafe.Unternehmenskette;
 
 /**
  * Abstraktion einer Entscheidung zur Durchführung von Marketing
+ * 
  * @author Marc
  */
 public class MarketingEntscheidung extends Entscheidung {
 
 	private double betrag;
-	
+	private Standort standort;
+	private double minBetrag = 1000.0; // TODO sinnvoller Start-Wert
+	private double maxBetrag = 10000.0; // TODO sinnvoller Start-Wert
+
 	/**
 	 * Erstellt eine neue Entscheidung zum Marketing
-	 * @param kette Unternehmenskette, die die Entscheidung getroffen hat
-	 * @param betrag Betrag, für den Marketing gemacht werden soll, dieser beeinflusst die Wahrscheinlichkeit, dass
-	 * 				 ein neuer Kunde das Unternehmen kennenlernt
+	 * 
+	 * @param kette
+	 *            Unternehmenskette, die die Entscheidung getroffen hat
+	 * @param betrag
+	 *            Betrag, für den Marketing gemacht werden soll, dieser
+	 *            beeinflusst die Wahrscheinlichkeit, dass ein neuer Kunde das
+	 *            Unternehmen kennenlernt
 	 */
-	public MarketingEntscheidung(Unternehmenskette kette, double betrag) {
+	public MarketingEntscheidung(Unternehmenskette kette, Standort standort,
+			double betrag) {
 		super(kette);
+		this.standort = standort;
 		this.betrag = betrag;
 	}
-	
+
 	/**
-	 * Führt die Entscheidung aus
-	 * Beeinflusst die Kunde an allen Standorten mit einer bestimmten Wahrscheinlichkeit, abhängig vom Betrag
+	 * Führt die Entscheidung aus Beeinflusst die Kunden am ausgewählten
+	 * Standort mit einer bestimmten Wahrscheinlichkeit, abhängig vom Betrag
 	 */
 	public void ausfuehren() {
 		if (betrag > 0) {
-			Vector<Filiale> filialenListe = kette.holeFilialenListe();
-			for (int i=0; i<filialenListe.size(); i++) {
-				Filiale filiale = filialenListe.get(i);
-				filiale.holeStandort().beeinflussenKunden(kette, holeWahrscheinlichkeit(betrag));
-			}	
+			standort.beeinflussenKunden(kette, holeWahrscheinlichkeit(betrag));
 		}
 	}
-	
+
 	/**
-	 * Bestimmt die Wahrscheinlichkeit, dass ein Kunde eine Unternehmenskette durch Marketing kennen lernt, abhängig
-	 * vom Marketing Budget
-	 * @param betrag Höhe des Marketing Budgets, welches die Wahrscheinlichkeit beeinflusst
+	 * Bestimmt die Wahrscheinlichkeit, dass ein Kunde eine Unternehmenskette am
+	 * Standort durch Marketing kennenlernt, abhängig vom Marketing Budget
+	 * 
+	 * @param betrag
+	 *            Höhe des Marketing Budgets, welches die Wahrscheinlichkeit
+	 *            beeinflusst
 	 * @return Wahrscheinlichkeit zwischen 0 und 1
 	 */
 	public double holeWahrscheinlichkeit(double betrag) {
-		// TODO 
-		// hier muss noch eine anständige funktion implementiert werden
-		// alternativ wird der marketing betrag einfach festgelegt und die wahrscheinlichkeit ist immer gleich
-		// werden automtisch alle standorte vom marketing betroffen? => NEIN STANDORT ÜBERGEBEN!
-		return 0.5;
+		double b = 0.2;
+		double maxWahrscheinlichkeit = 0.8;
+		double m = (maxWahrscheinlichkeit - b) / (maxBetrag - minBetrag);
+		double wahrscheinlichkeit;
+		if (betrag < minBetrag) {
+			wahrscheinlichkeit = 0.0;
+		} else if (betrag > maxBetrag) {
+			wahrscheinlichkeit = maxWahrscheinlichkeit;
+		} else {
+			wahrscheinlichkeit = (m * betrag) + b;
+		}
+		return wahrscheinlichkeit;
 	}
-
 }
