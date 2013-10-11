a4379389561a8eac67eb18fda3f5569a3e49b188
Natalie <nataliebuchner@aol.com>
2013-10-11 09:48:08 +0200

    HaendlerTest angelegt

---
+++ Planspiel/src/de/planspiel/test/HaendlerTest.java
@@ -0,0 +1,18 @@
+package de.planspiel.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+public class HaendlerTest {
+	
+	@Before 
+	public void erstelleSUT(){
+		
+	}
+	
+	@Test
+	public void testenErstellenAngebot(){
+		
+	}
+}
