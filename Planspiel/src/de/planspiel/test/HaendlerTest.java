7f3d69e00b5d22c489e3a152d08c267420196284
Natalie <nataliebuchner@aol.com>
2013-10-11 10:28:00 +0200

    HaendlerTest mit TestZufallszahlen

--- Planspiel/src/de/planspiel/test/HaendlerTest.java
+++ Planspiel/src/de/planspiel/test/HaendlerTest.java
@@ -4,15 +4,44 @@ import static org.junit.Assert.*;
 
 import org.junit.*;
 
+import de.planspiel.cafe.Haendler;
+import de.planspiel.cafe.Haendlertyp;
+import de.planspiel.cafe.PreisQualitaetVerhaeltnis;
+import de.planspiel.cafe.Produkttyp;
+import de.planspiel.spiel.Zufall;
+
 public class HaendlerTest {
+	Haendler haendler;
+	
+	@BeforeClass
+	public void vorbereiten(){
+		Zufall.setzeTestmodus(true);
+		Zufall.setzeTestQualitaet(0.8);
+	}
 	
 	@Before 
 	public void erstelleSUT(){
-		
+		haendler = new Haendler(Haendlertyp.Haendler1);
 	}
 	
 	@Test
 	public void testenErstellenAngebot(){
+		Double[] ekArray = new Double[Produkttyp.values().length];
+		for (int i = 0; i< ekArray.length;i++){  //Array erstellen, das erwartete EKPreise enthält
+			ekArray[i] = Produkttyp.values()[i].holeMaxEK() * PreisQualitaetVerhaeltnis.berechnenPreisFaktor(0.8);
+		}
 		
+		haendler.generierenAngebot();
+		for (int i = 0; i<haendler.holeProduktliste().size(); i++){
+			assertEquals(haendler.holeProduktliste().get(i).holeName(), Produkttyp.values()[i]);
+			assertEquals(haendler.holeProduktliste().get(i).holeQualitaet(), 0.8, 0.0);
+			assertEquals(haendler.holeProduktliste().get(i).holeEkpreis(), ekArray[i], 0.0);	
+		}
 	}
+	
+	@AfterClass
+	public void beenden(){
+		Zufall.setzeTestmodus(false);
+	}
+	
 }
