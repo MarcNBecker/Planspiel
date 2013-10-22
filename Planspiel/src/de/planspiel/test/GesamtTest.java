eac9987391ca61922d18b2fcc4eb6d97d9073e53
Natalie <nataliebuchner@aol.com>
2013-10-22 12:08:24 +0200

    Fehler entfernt - gesamterTest

--- Planspiel/src/de/planspiel/test/GesamtTest.java
+++ Planspiel/src/de/planspiel/test/GesamtTest.java
@@ -93,8 +93,24 @@ public class GesamtTest {
 					spiel.hinzufuegenRundenEntscheidung(e7);
 					spiel.hinzufuegenRundenEntscheidung(e8);
 					
-					//AB HIER WEITER!!!!
-					
+					//Für UKette2:
+					FilialeEroeffnenEntscheidung e9 = new FilialeEroeffnenEntscheidung(ukette1, spiel.holeStandortListe().get(1), 3);
+					KreditEntscheidung e10 = new KreditEntscheidung(ukette1, 100.0);
+					RohstoffEntscheidung e11 = new RohstoffEntscheidung(ukette1, spiel.holeHaendlerListe().get(2), Produkttyp.KAFFEE, 30);
+					RohstoffEntscheidung e12 = new RohstoffEntscheidung(ukette1, spiel.holeHaendlerListe().get(2), Produkttyp.TEE, 30);
+					RohstoffEntscheidung e13 = new RohstoffEntscheidung(ukette1, spiel.holeHaendlerListe().get(2), Produkttyp.KUCHEN, 40);
+					VerkaufspreisEntscheidung e14 = new VerkaufspreisEntscheidung(ukette1, Produkttyp.KAFFEE, 3.00);
+					VerkaufspreisEntscheidung e15 = new VerkaufspreisEntscheidung(ukette1, Produkttyp.TEE, 2.50);
+					VerkaufspreisEntscheidung e16 = new VerkaufspreisEntscheidung(ukette1, Produkttyp.KUCHEN, 5.00);
+					spiel.hinzufuegenRundenEntscheidung(e9);
+					spiel.hinzufuegenRundenEntscheidung(e10);
+					spiel.hinzufuegenRundenEntscheidung(e11);
+					spiel.hinzufuegenRundenEntscheidung(e12);
+					spiel.hinzufuegenRundenEntscheidung(e13);
+					spiel.hinzufuegenRundenEntscheidung(e14);
+					spiel.hinzufuegenRundenEntscheidung(e15);
+					spiel.hinzufuegenRundenEntscheidung(e16);
+										
 					// Entscheidungen ausführen
 					for(int i=0; i<spiel.holeRundenEntscheidungen().size(); i++){
 						Entscheidung e = spiel.holeRundenEntscheidungen().get(i);
@@ -136,8 +152,11 @@ public class GesamtTest {
 						}
 					}
 					
+					//Hier Werte überprüfen - AUS REPORT??? am besten, oder?
+					
 					// Runde neu setzen
-					spiel.setzeAktuelleRunde(spiel.holeAktuelleRunde() + 1);
+					spiel.setzeAktuelleRunde(spiel.holeAktuelleRunde() + 1);					
+					
 				
 				//Rundenorganisation - Runde 2	
 					// Kreditlaufzeit setzen Achtung - hier nicht sehr lang, da Nur 2 Runden gespielt werden
@@ -160,7 +179,42 @@ public class GesamtTest {
 					// Entscheidungs HashMap initialisieren
 					rundenEntscheidungen = new Vector<Entscheidung>();
 								
-					new EntscheidungTreffenGUI().run();
+					//Entscheidungen treffen - hier im Test - WERTE NOCH ÄNDERN!!! AUCH UNTEN
+					//Für UKette1:
+					e1 = new FilialeEroeffnenEntscheidung(ukette1, spiel.holeStandortListe().get(1), 5);
+					e2 = new KreditEntscheidung(ukette1, 200.0);
+					e3 = new RohstoffEntscheidung(ukette1, spiel.holeHaendlerListe().get(1), Produkttyp.KAFFEE, 50);
+					e4 = new RohstoffEntscheidung(ukette1, spiel.holeHaendlerListe().get(1), Produkttyp.TEE, 20);
+					e5 = new RohstoffEntscheidung(ukette1, spiel.holeHaendlerListe().get(1), Produkttyp.KUCHEN, 30);
+					e6 = new VerkaufspreisEntscheidung(ukette1, Produkttyp.KAFFEE, 4.00);
+					e7 = new VerkaufspreisEntscheidung(ukette1, Produkttyp.TEE, 3.00);
+					e8 = new VerkaufspreisEntscheidung(ukette1, Produkttyp.KUCHEN, 2.50);
+					spiel.hinzufuegenRundenEntscheidung(e1);
+					spiel.hinzufuegenRundenEntscheidung(e2);
+					spiel.hinzufuegenRundenEntscheidung(e3);
+					spiel.hinzufuegenRundenEntscheidung(e4);
+					spiel.hinzufuegenRundenEntscheidung(e5);
+					spiel.hinzufuegenRundenEntscheidung(e6);
+					spiel.hinzufuegenRundenEntscheidung(e7);
+					spiel.hinzufuegenRundenEntscheidung(e8);
+					
+					//Für UKette2:
+					e9 = new FilialeEroeffnenEntscheidung(ukette1, spiel.holeStandortListe().get(1), 3);
+					e10 = new KreditEntscheidung(ukette1, 100.0);
+					e11 = new RohstoffEntscheidung(ukette1, spiel.holeHaendlerListe().get(2), Produkttyp.KAFFEE, 30);
+					e12 = new RohstoffEntscheidung(ukette1, spiel.holeHaendlerListe().get(2), Produkttyp.TEE, 30);
+					e13 = new RohstoffEntscheidung(ukette1, spiel.holeHaendlerListe().get(2), Produkttyp.KUCHEN, 40);
+					e14 = new VerkaufspreisEntscheidung(ukette1, Produkttyp.KAFFEE, 3.00);
+					e15 = new VerkaufspreisEntscheidung(ukette1, Produkttyp.TEE, 2.50);
+					e16 = new VerkaufspreisEntscheidung(ukette1, Produkttyp.KUCHEN, 5.00);
+					spiel.hinzufuegenRundenEntscheidung(e9);
+					spiel.hinzufuegenRundenEntscheidung(e10);
+					spiel.hinzufuegenRundenEntscheidung(e11);
+					spiel.hinzufuegenRundenEntscheidung(e12);
+					spiel.hinzufuegenRundenEntscheidung(e13);
+					spiel.hinzufuegenRundenEntscheidung(e14);
+					spiel.hinzufuegenRundenEntscheidung(e15);
+					spiel.hinzufuegenRundenEntscheidung(e16);
 					
 					// Entscheidungen ausführen
 					for(int i=0; i<spiel.holeRundenEntscheidungen().size(); i++){
