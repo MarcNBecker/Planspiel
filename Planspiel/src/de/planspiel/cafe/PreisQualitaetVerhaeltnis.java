582c997d1c0766f504fe71290f76e2f5b8d5ec6c
Kathi <ann-kathrin.gessat@web.de>
2013-10-22 12:00:51 +0200

    Methodenname geändert

--- Planspiel/src/de/planspiel/cafe/PreisQualitaetVerhaeltnis.java
+++ Planspiel/src/de/planspiel/cafe/PreisQualitaetVerhaeltnis.java
@@ -13,7 +13,7 @@ public class PreisQualitaetVerhaeltnis {
 		if (x < 0.1) {
 			x = 0.1;
 		}
-		return (berechne(x, a) / berechne(1.0, a));
+		return (berechnen(x, a) / berechnen(1.0, a));
 	}
 	
 	/**
@@ -22,7 +22,7 @@ public class PreisQualitaetVerhaeltnis {
 	 * @param a Faktor zur Beeinflussung
 	 * @return Preisfaktor
 	 */
-	private static double berechne(double x, double a) {
+	private static double berechnen(double x, double a) {
 		return a * Math.log(x+1.0) + 2.0 * a * Math.pow(x, 5.0) - a * Math.pow(x, 2.0) - 0.5 * Math.pow(Math.E, Math.pow(x, 9) + Math.pow(x, 3) - x * (1/a)) + 0.5;
 		//a*ln(x+1)+2*a*x^5-a*x^2-0.5*e^(x^9+x^3-(1/a)*x)+0.5
 	}
