744948bb079f9bba3dd8d4cb626a7048c22322a3
Daniel <DanielDegraf@googlemail.com>
2013-10-28 16:04:20 +0100

    FilialeTest funktioniert immer noch nicht.

--- Planspiel/src/de/planspiel/test/FilialeTest.java
+++ Planspiel/src/de/planspiel/test/FilialeTest.java
@@ -7,20 +7,26 @@ import org.junit.*;
 import de.planspiel.cafe.Filiale;
 import de.planspiel.cafe.Produkt;
 import de.planspiel.cafe.Produkttyp;
+import de.planspiel.cafe.Report;
 import de.planspiel.cafe.Standort;
 import de.planspiel.cafe.Standorttyp;
 import de.planspiel.cafe.Unternehmenskette;
+import de.planspiel.spiel.Spiel;
 
 public class FilialeTest {
 
 	private Standort standort;
 	private Unternehmenskette ukette;
 	private Filiale fil1;
-	
+	private Spiel spiel = new Spiel();
+	private Report report;
 	@Before
 	public void erstelleSUT() {
+		report = new Report(0, ukette);
+		
 		standort = new Standort(Standorttyp.Standort1);
 		ukette  = new Unternehmenskette("KetteNummer1");
+		
 		fil1 = new Filiale(standort, ukette);
 		fil1.setzeMitarbeiter(1);
 		fil1.initialisierenKapazitaet();
@@ -35,7 +41,7 @@ public class FilialeTest {
 		fil1.verkaufen(Produkttyp.KAFFEE, 10);
 		//Produkt vergleichsProdukt = new Produkt(Produkttyp.KAFFEE, 20);
 		//assertEquals(ukette.holeLager().suchenProdukt(Produkttyp.KAFFEE).holeMenge(), 10);
-		assertEquals(fil1.holeFreieKapazitaet(),0);
+		assertEquals(fil1.holeFreieKapazitaet(),99);
 	}
 	
 	@Test
